b9d22a97cf59fb73150ce92d9200d6f2a26eeb47
btchoum <bertrand.tchoumkeu@gmail.com>
2021-01-05 19:53:06 -0600

    Add project files.

---
+++ BugTracker/BugTracker.Web/Controllers/BugsApiController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace BugTracker.Web.Controllers
+{
+    [RoutePrefix("api/bugs")]
+    public class BugsApiController : ApiController
+    {
+
+        [HttpGet]
+        [Route("")]
+        public IHttpActionResult GetBugs()
+        {
+            return Ok();
+        }
+    }
+}
