3e5a5b49f07a01f38727a01f9590cc05469207ef
btchoum <bertrand.tchoumkeu@gmail.com>
2021-01-05 19:55:59 -0600

    add shared broject

---
+++ BugTracker/BugTracker.Data/Bug.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BugTracker.Data
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Details { get; set; }
+        public string UserEmail { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
