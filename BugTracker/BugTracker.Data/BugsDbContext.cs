ea11b2902f6d42865129865a402e682426300c15
btchoum <bertrand.tchoumkeu@gmail.com>
2021-01-05 20:18:09 -0600

    looking good with the database

--- BugTracker/BugTracker.Data/BugsDbContext.cs
+++ BugTracker/BugTracker.Data/BugsDbContext.cs
@@ -4,6 +4,12 @@ namespace BugTracker.Data
 {
     public class BugsDbContext : DbContext
     {
+        public BugsDbContext(string connectionString) 
+            : base(connectionString)
+        {
+            Database.SetInitializer(new NullDatabaseInitializer<BugsDbContext>());
+        }
+        
         public DbSet<Bug> Bugs { get; set; }
     }
 }
--- BugTracker/BugTracker.Web.Tests/Controllers/BugsApiControllerTest.cs
+++ BugTracker/BugTracker.Web.Tests/Controllers/BugsApiControllerTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Web.Http.Results;
 using BugTracker.Data;
 using BugTracker.Web.Controllers;
@@ -12,11 +13,39 @@ namespace BugTracker.Web.Tests.Controllers
         [TestMethod]
         public void GetTickets()
         {
-            var controller = new BugsApiController(new BugsDbContext());
+            
+            var connectionString = "Server=(localdb)\\mssqllocaldb;Database=BugsDbContextTests";
+            
+            var context = new BugsDbContext(connectionString);
+            context.Database.CreateIfNotExists();
+            
+            context.Database.ExecuteSqlCommand("DELETE FROM dbo.Bugs");
+            context.Bugs.Add(new Bug
+            {
+                Title = "some title 1",
+                Details = "some details 1",
+                Created = DateTime.UtcNow,
+                Updated = DateTime.UtcNow,
+                UserEmail = "test@test.com"
+            });
+            context.Bugs.Add(new Bug
+            {
+                Title = "some title 2",
+                Details = "some details 2",
+                Created = DateTime.UtcNow,
+                Updated = DateTime.UtcNow,
+                UserEmail = "test@test.com"
+            });
+
+            context.SaveChanges();
+
+            var controller = new BugsApiController(new BugsDbContext(connectionString));
             
             var result = controller.GetBugs() as OkNegotiatedContentResult<List<Bug>>;
             
             Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Content);
+            Assert.AreEqual(2, result.Content.Count);
         }
     }
 }
